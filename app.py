9cc0d128a32f678dc7a7ac5a602320a941fa5caf
jdd123455-sketch <jdd123455@gmail.com>
2026-08-25 19:42:03 +0530

    fix proxy order

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, session, redirect, render_template_string, request, jsonify
 from authlib.integrations.flask_client import OAuth
+from werkzeug.middleware.proxy_fix import ProxyFix
 import sqlite3, random, string, os
 from datetime import datetime
-from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 app.secret_key = "hsl_corp_final_2026_pro"
+
 
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
